8e1690b0a991b974a382ce09b594eec740a88388
laury <laurysss245@gmail.com>
2024-06-17 15:01:40 +0300

    Create apollo server with express middleware

---
+++ Server/build/index.ts
@@ -0,0 +1,57 @@
+import log from 'fancy-log';
+import 'reflect-metadata';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
+import cookieParser from 'cookie-parser';
+import { ApolloServer } from 'apollo-server-express';
+import * as asc from 'apollo-server-core'
+
+async function bootstrap() {
+    log("Building schema");
+
+    // build the schema
+    const schema = await buildSchema({
+    });
+
+    log("Initializing express");
+
+    // init express
+    const app = express();
+
+    app.use(cookieParser());
+
+    app.set('trust proxy', true)
+
+    log("Initializing apollo server");
+
+    // create the apollo server
+    const server = new ApolloServer({
+        schema,
+        context: (ctx: any) => {
+            
+
+            return ctx;
+        },
+        plugins: [
+            asc.ApolloServerPluginLandingPageLocalDefault()
+        ],
+    });
+
+    await server.start();
+
+    log("Server started");
+    log("Applying middleware to server");
+
+    // apply middleware to server
+    server.applyMiddleware({app});
+
+    app.listen({port: 9049}, () => {
+        log("App is listening on http://localhost:9049/graphql");
+    });
+
+    log("System fully initialized");
+}
+
+bootstrap().catch((err) => {
+    log(err);
+})
